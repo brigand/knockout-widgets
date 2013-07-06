967a17f363b92b99b6cfe2041bf36523bf529a0a
Frankie Bagnardi <fbagnardi@gmail.com>
2013-07-05 18:12:53 -0700

    build version 0.0.1

--- build/knockout.widgets.dev.js
+++ build/knockout.widgets.dev.js
@@ -16,6 +16,8 @@
         var $parent = opt.parent === -1 ? $el.parent() : $(opt.parent);
         var $document = $(document);
 
+        $el.offset({left: 0});
+
         return $el.on("mousedown", function (e) {
             $family = $($el, $el.parents());
             $document.one("mouseup", function () {
@@ -62,12 +64,62 @@
     }
 })
     (jQuery);
+ko.widgets = ko.widgets || {};
+
+ko.widgets.once = function(callback){
+    var action = function(){
+        if (!action.happened) {
+            action.happened = true;
+            return callback.apply(this, arguments);
+        }
+        else {
+            return action._then.apply(this, arguments);
+        }
+    };
+
+    /* Sets what should be called after the first call */
+    action.then = function(next){
+        action._then = next;
+        return action;
+    };
+
+    action.happened = false;
+    action._then = function(){};
+
+    return action;
+};
+ko.observable.fn.toggle = function(){
+    var observable = this;
+
+    // Initially set it to the boolean version of itself
+    observable(!!observable());
+
+    return (function(){
+        // When called, flip the value and store it back in the variable
+        observable(!observable());
+    });
+};
+
+ko.subscribable.fn.append = function(what){
+    var observable = this;
+    return ko.computed(function(){
+        return observable() + what;
+    });
+};
+
+ko.debug = function(a, b, c, d){
+    console.log("DEBUG", a, b, c, d);
+    return '';
+};
 var unwrap = ko.utils.unwrapObservable;
 var identity = function(x) { return x; };
 
+/* Simple slider
+
+ */
 ko.bindingHandlers.slider = {
     init: function(element, valueAccessor, allBindingsAccessor) {
-        var value = unwrap(valueAccessor()),
+        var value = valueAccessor(),
             allBindings = allBindingsAccessor(),
             min = unwrap(allBindings.sliderMin) || 0,
             max = unwrap(allBindings.sliderMax) || 1,
@@ -76,9 +128,7 @@ ko.bindingHandlers.slider = {
         if (typeof round !== "function")
             round = round ? Math.round : identity;
 
-        percent = round(
-            ko.utils.unwrapObservable(value) / (max - min)
-        );
+        percent = round(unwrap(value)) / (max - min);
 
         // Used to prevent changing value during a drag
         element.dataset.protected = false;
@@ -87,7 +137,7 @@ ko.bindingHandlers.slider = {
             callback: function(newPercent){
                 valueAccessor()((max-min) * newPercent + min);
             }
-        }).slideto(round(unwrap(percent)));
+        });
     },
     update: function(element, valueAccessor, allBindingsAccessor) {
         var value = valueAccessor(),
@@ -97,19 +147,48 @@ ko.bindingHandlers.slider = {
             round = unwrap(allBindings.round),
             percent = 0;
 
+        percent = unwrap(value) / (max - min);
+
         if (typeof round !== "function")
             round = round ? Math.round : identity;
 
         // Don't update if we're sliding
         if(element.dataset.protected === "true") {
-            console.log('protect', percent);
             return;
         }
 
-        percent = ko.utils.unwrapObservable(value) / (max - min);
 
         value(round(value()));
 
         $(element).slideto(percent);
     }
 };
+
+ko.bindingHandlers.slideVisible = visibility($.prototype.slideDown, $.prototype.slideUp);
+ko.bindingHandlers.fadeVisible = visibility($.prototype.fadeIn, $.prototype.fadeOut);
+
+function visibility(show, hide, kind){
+    var handle = function(element, valueAccessor, allBindingsAccessor) {
+            var value = unwrap(valueAccessor()),
+                allBindings = allBindingsAccessor(),
+                speed = allBindings.speed || 400;
+
+            if (value) {
+                show.call($(element), speed);
+            }
+            else {
+                hide.call($(element), speed);
+            }
+    };
+
+    if (typeof kind === "undefined" || kind === "both") {
+        return {
+            init: visibility($.prototype.show, $.prototype.hide, "init"),
+            update: handle
+        };
+    }
+    else if (kind === "init" || kind === "update") {
+        return handle;
+    }
+}
+
